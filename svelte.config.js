f766821a28b834b5edae3029e65a2eaf9ae99a97
Ewan Croft <git@ewancroft.uk>
2026-02-24 00:55:45 +0000

    chore: move to Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ pages: 'dist', assets: 'dist', fallback: '404.html' })
+		adapter: adapter()
 	}
 };
 
